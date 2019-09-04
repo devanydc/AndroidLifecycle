abcd8fda2dfc4cf4725db00c3f051590cca9524b
Devany <devanydc>
2019-09-04 16:14:55 +0700

    Push2

---
+++ app/src/main/java/id/ac/polinema/androidlifecycle/SimpleActivity.java
@@ -0,0 +1,27 @@
+package id.ac.polinema.androidlifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SimpleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+    }
+
+    public void handleSubmit(View view) {
+        String name = nameInput.getText().toString();
+        outputText.setText("Hello " + name);
+    }
+    private EditText nameInput;
+
+    private TextView outputText;
+}
